49929fec8df0526233681c549a576e89dadcf860
Lauttd <lautaroayala123090@gmail.com>
2025-07-10 16:25:29 -0300

    se aplicaron las validaciones

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -1,7 +1,30 @@
 import { languagemodel } from "../models/language.model.js";
 
+//Creamos un lenguaje
 export const createLanguages = async (req, res) => {
   try {
+    const { name } = req.body;
+    const { paradimg } = req.body;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ message: "El campo 'name' es obligatorio." });
+    }
+
+    if (name === " ") {
+      return res.status(400).json({ message: "el name no puede estar vacio" });
+    }
+
+    if (paradimg === " ") {
+      return res.status(400).json({ message: "el paradigm es obligario" });
+    }
+
+    const existeLanguage = await languagemodel.findOne({ where: { name } });
+    if (existeLanguage) {
+      return res.status(409).json({ message: "El nombre ya está en uso." });
+    }
+
     const crearLanguages = await languagemodel.create(req.body);
     return res.status(200).json(crearLanguages);
   } catch (error) {
@@ -11,6 +34,7 @@ export const createLanguages = async (req, res) => {
   }
 };
 
+//Obtenemos todos los lenguajes
 export const getAllLanguages = async (req, res) => {
   try {
     const obtenerLanguage = await languagemodel.findAll(req.body);
@@ -22,6 +46,7 @@ export const getAllLanguages = async (req, res) => {
   }
 };
 
+//Obtenemos los lenguajes por id
 export const getByIdLanguages = async (req, res) => {
   try {
     const obtenerLanguagesId = await languagemodel.findByPk(req.params.id);
@@ -33,8 +58,28 @@ export const getByIdLanguages = async (req, res) => {
   }
 };
 
+//Actualizamos los lenguajes
 export const updateLanguages = async (req, res) => {
   try {
+    const { name } = req.body;
+
+    const lenguaje = await languagemodel.findByPk(req.params.id);
+    if (!lenguaje) {
+      return res.status(404).json({ message: "Lenguaje no encontrado." });
+    }
+
+    if (name !== undefined) {
+      return res
+        .status(400)
+        .json({ message: "El campo 'name' no puede estar vacío." });
+    }
+
+    if (name) {
+      const existeLanguage = await languagemodel.findOne({
+        where: { name, id: { $ne: req.params.id } },
+      });
+    }
+
     const [updated] = await languagemodel.update(req.body, {
       where: { id: req.params.id },
     });
@@ -49,6 +94,7 @@ export const updateLanguages = async (req, res) => {
   }
 };
 
+//Eliminar lenguaje
 export const deleteLanguages = async (req, res) => {
   try {
     const borrarLanguage = await languagemodel.destroy({
